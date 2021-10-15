e349997159132ed6d5f35c0a969264167e46fc80
sawa777yu <sawa.oranger7@gmail.com>
2021-10-15 08:44:30 +0000

    [Add]admin_user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,23 @@
 class Admin::UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.save(user_params)
+    redirect_to admin_root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :our_answers_id, :email, :is_deleted)
+  end
+
 end
