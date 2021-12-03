68f73cb2422ab643eb6e85cb4c5dad3908cc6616
sawa777yu <sawa.oranger7@gmail.com>
2021-12-03 07:50:50 +0000

    [Add] マイページにソート機能

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def sort_order(column)
+    direction = (column == sort_column && sort_direction == "desc") ? "asc" : "desc"
+    link_to content_tag(:i,"",class: "fas fa-sort"), { column: column, direction: direction }
+  end
+
 end
